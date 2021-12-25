e550c2579015ea26e7fd962e859df91f2e6a09b8
karlpederolesen <96655854+karlpederolesen@users.noreply.github.com>
2021-12-25 18:18:45 +0100

    Opdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,59 @@
+input.onGesture(Gesture.Shake, function () {
+    udfald = randint(1, 6)
+    if (udfald == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (udfald == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . . . .
+            . . . # .
+            . . . . .
+            `)
+    } else if (udfald == 3) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . # . .
+            . . . # .
+            . . . . .
+            `)
+    } else if (udfald == 4) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+    } else if (udfald == 5) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . # . # .
+            . # . # .
+            . . . . .
+            `)
+    }
+})
+let udfald = 0
+basic.showString("Terning")
+basic.pause(500)
+basic.clearScreen()
 basic.forever(function () {
 	
 })
